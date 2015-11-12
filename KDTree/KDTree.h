d465cb86d7a6ef4cc3fdacaee41f1cb7ebac4b6c
atul_kumar_daqri <atul.kumar@daqri.com>
2015-11-11 22:40:51 -0800

    kd tree

--- KDTree/KDTree.h
+++ KDTree/KDTree.h
@@ -14,6 +14,8 @@
 #include "BoundedPQueue.h"
 #include <stdexcept>
 #include <cmath>
+#include <limits>
+#include <set>
 
 // "using namespace" in a header file is conventionally frowned upon, but I'm
 // including it here so that you may use things like size_t without having to
@@ -100,27 +102,238 @@ public:
     ElemType kNNValue(const Point<N>& key, size_t k) const;
 
 private:
-    // TODO: Add implementation details here.
+    struct Node {
+        Point<N> key;
+        int level;
+        ElemType value;
+        
+        Node* left;
+        Node* right;
+        
+        int getSplitIndex(){
+            return level%N;
+        }
+    };
+    
+    Node* root;
+    size_t _size;
+    
+    Node* findNode(const Point<N>& pt) const;
+    Node* insertAndReturn(const Point<N>& pt, const ElemType& value);
+    void kNNValue(const Point<N>& key, Node* curr,
+                      BoundedPQueue<Node*>& bpq) const;
+    
+    void deleteTree(Node* curr);
+    Node* copyTree(Node* curr);
 };
+template <size_t N, typename ElemType>
+void KDTree<N, ElemType>::deleteTree(Node* curr){
+    if(curr == nullptr){
+        return;
+    }
+    deleteTree(curr->left);
+    deleteTree(curr->right);
+    delete curr;
+}
+
+template <size_t N, typename ElemType>
+typename KDTree<N, ElemType>::Node*
+KDTree<N, ElemType>::copyTree(Node* curr){
+    if(curr == nullptr){
+        return nullptr;
+    }
+    
+    Node* n = new Node();
+    n->key = curr->key;
+    n->value = curr->value;
+    n->level = curr->level;
+    n->left = copyTree(curr->left);
+    n->right = copyTree(curr->right);
+    
+    return n;
+}
+
+template <size_t N, typename ElemType>
+typename KDTree<N, ElemType>::Node*
+KDTree<N, ElemType>::findNode(const Point<N>& pt) const{
+    Node* curr= root;
+    while(curr != nullptr){
+        if(pt ==curr->key){
+            return curr;
+        }
+        int splitIndex = curr->getSplitIndex();
+        if(pt[splitIndex] > curr->key[splitIndex]){
+            curr = curr->right;
+        }else{
+            curr = curr->left;
+        }
+    }
+    return nullptr;
+}
 
 /** KDTree class implementation details */
 
 template <size_t N, typename ElemType>
 KDTree<N, ElemType>::KDTree() {
-    // TODO: Fill this in.
+    root = nullptr;
+    _size = 0;
 }
 
 template <size_t N, typename ElemType>
 KDTree<N, ElemType>::~KDTree() {
-    // TODO: Fill this in.
+    deleteTree(root);
+    root = nullptr;
+    _size = 0;
 }
 
 template <size_t N, typename ElemType>
 size_t KDTree<N, ElemType>::dimension() const {
-    // TODO: Fill this in.
-    return 0;
+    return N;
 }
 
-// TODO: finish the implementation of the rest of the KDTree class
+template <size_t N, typename ElemType>
+bool KDTree<N, ElemType>::contains(const Point<N>& pt) const{
+    return findNode(pt) != nullptr;
+}
+template <size_t N, typename ElemType>
+size_t KDTree<N, ElemType>::size() const{
+    return _size;
+}
+template <size_t N, typename ElemType>
+bool KDTree<N, ElemType>::empty() const{
+    return size() == 0;
+}
+template <size_t N, typename ElemType>
+const ElemType& KDTree<N, ElemType>::at(const Point<N>& pt) const{
+    Node* n = findNode(pt);
+    if(n == nullptr){
+        throw out_of_range("Point does not exist");
+    }
+    return n->value;
+}
+template <size_t N, typename ElemType>
+ElemType& KDTree<N, ElemType>::at(const Point<N>& pt) {
+    const KDTree<N, ElemType>& constThis = *this;
+    return const_cast<ElemType&>(constThis.at(pt));
+}
+
+template <size_t N, typename ElemType>
+void KDTree<N, ElemType>::insert(const Point<N>& pt, const ElemType& value){
+    insertAndReturn(pt, value);
+}
+template <size_t N, typename ElemType>
+typename KDTree<N, ElemType>::Node*
+KDTree<N, ElemType>::insertAndReturn(const Point<N>& pt, const ElemType& value){
+    Node* curr= root;
+    Node* pre = nullptr;
+    int level = 0;
+    while(curr != nullptr){
+        level++;
+        Node* nextNode = nullptr;
+        if(pt ==curr->key){
+            curr->value = value;
+            return curr;
+        }
+        int splitIndex = curr->getSplitIndex();
+        if(pt[splitIndex] > curr->key[splitIndex]){
+            nextNode = curr->right;
+        }else{
+            nextNode = curr->left;
+        }
+        pre = curr;
+        curr = nextNode;
+    }
+    Node* n = new Node();
+    n->key = pt;
+    n->value = value;
+    n->level = level;
+    n->left = nullptr;
+    n->right = nullptr;
+    
+    if (curr == root) {
+        root = n;
+    } else {
+        int splitIndex = pre->getSplitIndex();
+        pt[splitIndex] > pre->key[splitIndex] ? pre->right = n : pre->left = n;
+    }
+    _size++;
+    return n;
+}
+
+template <size_t N, typename ElemType>
+ElemType& KDTree<N, ElemType>::operator[](const Point<N>& pt){
+    Node* n = findNode(pt);
+    if(n == nullptr){
+        n = insertAndReturn(pt, ElemType());
+    }
+    return n->value;
+}
+
+template <size_t N, typename ElemType>
+ElemType KDTree<N, ElemType>::kNNValue(const Point<N>& key, size_t k) const{
+    BoundedPQueue<Node*> bpq(k);
+    
+    kNNValue(key, root, bpq);
+    
+    std::multiset<ElemType> values;
+    while(!bpq.empty()){
+        Node* minVal = bpq.dequeueMin();
+        values.insert(minVal->value);
+    }
+    ElemType best;
+    size_t bestFrequency = 0;
+    for(auto it = values.begin(); it !=values.end(); ++it) {
+        if (values.count(*it) > bestFrequency) {
+            best = *it;
+            bestFrequency = values.count(*it);
+        }
+    }
+    return best;
+}
+
+template <size_t N, typename ElemType>
+void KDTree<N, ElemType>::kNNValue(const Point<N>& key, Node* curr,
+                                       BoundedPQueue<Node*>& bpq) const{
+    if(curr == nullptr){
+        return;
+    }
+    double newDist = Distance(key, curr->key);
+    
+    bpq.enqueue(curr, newDist);
+    
+    int splitIndex = curr->getSplitIndex();
+    Node* nextNode = nullptr;
+    Node* otherNode = nullptr;
+    if(key[splitIndex] > curr->key[splitIndex]){
+        nextNode = curr->right;
+        otherNode = curr->left;
+    }else{
+        otherNode = curr->right;
+        nextNode = curr->left;
+    }
+    kNNValue(key, nextNode, bpq);
+    double bestDist = bpq.worst();
+    if(bpq.size() < bpq.maxSize() || fabs(key[splitIndex] - curr->key[splitIndex]) < bestDist){
+        kNNValue(key, otherNode, bpq);
+    }
+}
+
+template <size_t N, typename ElemType>
+KDTree<N, ElemType>::KDTree(const KDTree& rhs){
+    root = copyTree(rhs.root);
+    _size = rhs._size;
+}
+
+template <size_t N, typename ElemType>
+KDTree<N, ElemType>& KDTree<N, ElemType>::operator=(const KDTree& rhs){
+    if(&rhs != this){
+        deleteTree(root);
+        root = copyTree(rhs.root);
+        _size = rhs._size;
+    }
+    return *this;
+}
+
+
 
 #endif // KDTREE_INCLUDED
