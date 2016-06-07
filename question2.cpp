a1a79c5b941c320cc8ae6d90e8b22da9496e7b17
atulkum <atulcst@gmail.com>
2016-06-06 20:39:03 -0700

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -1,25 +1,21 @@
 struct Node{
-    int id;
-    int level;
-    int t;
+    int id,level,t,nChild;
+    double exp,expParent;
     Node* parent;
-    double exp;
-    double expParent;
     std::vector<Node*> children;
+    
     Node(int _id, int _level, int _t, Node* _parent){
-        id = _id;
-        level = _level;
-        t = _t;
-        parent = _parent;
+        id = _id; level = _level;  t = _t; parent = _parent;
     }
     void calculateExpect(){
+        nChild = children.size();
         exp = t;
         if(!children.empty()){
             double sum = 0;
             for(Node* node: children){
                 sum += node->exp;
             }
-            exp += sum/children.size();
+            exp += sum/nChild;
         }
     }
     void calculateExpectWithParent(){
@@ -27,14 +23,17 @@ struct Node{
         if(parent == nullptr){
             expParent = exp;
         }else{
-            double newParentExp = (parent->expParent - parent->t)*(parent->children.size()+1);
+            double newParentExp = (parent->expParent - parent->t)*parent->nChild;
             newParentExp -= exp;
-            if(parent->children.size() > 0){
-                newParentExp /= parent->children.size();
+            if(parent->nChild > 1){
+                newParentExp /= (parent->nChild-1);
             }
             newParentExp += parent->t;
+            
+            nChild = nChild+1;
+            
             expParent = (exp - t)*children.size() + newParentExp;
-            expParent /= (children.size()+1);
+            expParent /= nChild;
             expParent += t;
             
         }
@@ -44,46 +43,55 @@ int relatedQuestions(int n, std::vector<int> t, std::vector<std::vector<int>> ed
     using namespace std;
     map<int, vector<int>> m;
     map<int, int> parent;
+    int rootId = n-1;
     for(vector<int> e: edges){
-        m[e[0]].push_back(e[1]);
+        int from = e[0];
+        int to = e[1];
+        m[from].push_back(to);
+        m[to].push_back(from);
+        int minId = min(from, to);
+        if( minId< rootId){
+            rootId = minId;
+        }
     }
-    
-    Node* root = new Node(0, 0, t[0], nullptr);
+    Node* root = new Node(rootId, 0, t[rootId], nullptr);
     queue<Node*> q;
     q.push(root);
     map<int, vector<Node*>> levelmap;
+    set<int> visited;
     int level = -1;
     while(!q.empty()){
         Node* node = q.front();
         q.pop();
+        visited.insert(node->id);
         if(node->level > level){
             level = node->level;
         }
         levelmap[node->level].push_back(node);
         vector<int> children = m[node->id];
         for(int c : children){
-            Node* newNode = new Node(c, node->level +1, t[c], node);
-            q.push(newNode);
-            node->children.push_back(newNode);
+            if(visited.count(c) ==0){
+                Node* newNode = 
+                    new Node(c, node->level +1, t[c], node);
+                q.push(newNode);
+                node->children.push_back(newNode);
+            }
         }
     }
     
     for(int i = level; i >=0 ; --i){
         vector<Node*> levMap = levelmap[i];
-        
         for(Node * node: levMap){
-            cout << node->id << " ";
             node->calculateExpect();
         }
-        cout <<endl;
     }
-    double minx = INT_MAX;
+    
+    double minx = std::numeric_limits<double>::max();
     int mini = -1;
     for(int i = 0; i <= level ; ++i){
         vector<Node*> levMap = levelmap[i];
         for(Node * node: levMap){
             node->calculateExpectWithParent();
-            //cout << node->id << " " << node->expParent<< " "<< node->exp << endl;
             if(node->expParent <minx ){
                 minx = node->expParent;
                 mini = node->id;
