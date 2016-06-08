057004c79bed12cd4e76f77ab781e0e815ba0432
atulkum <atulcst@gmail.com>
2016-06-08 13:52:51 -0700

    Update ontology.cpp

--- ontology.cpp
+++ ontology.cpp
@@ -2,10 +2,9 @@ struct TreeNode{
     std::string topic;
     std::vector<TreeNode*> children;
     TreeNode(std::string _topic):topic(_topic){}
-    
 };
 
-bool isPrefix(std::string  ques, std::string query){
+bool isPrefix(std::string ques, std::string query){
     int i = 0;
     while(i < std::min(ques.size(), query.size()) 
           && ques[i] == query[i]){
@@ -13,63 +12,89 @@ bool isPrefix(std::string  ques, std::string query){
     }
     return i == query.size();
 }
-void countQuery(TreeNode* root, std::string q, 
-                std::map<string, TreeNode*>& topic2question, 
-                std::vector<int>& ret, int i){
+int countQuery(TreeNode* root, std::string query, 
+                std::map<std::string, std::vector<std::string>>& topic2question){
     if(root == nullptr){
-        return;
+        return 0;
     }
-    if(isPrefix(topic2question[root->topic], q)){
-        ret[i]++;
+    int ret = 0;
+    for(std::string question: topic2question[root->topic]){
+        std::cout <<root->topic<< " " <<question << " countQuery " << query <<std::endl ;
+        if(isPrefix(question, query)){
+            ret++;
+        }
     }
     for(TreeNode* child:root->children){
-        countQuery(child, q, topic2question, ret, i);
+        ret += countQuery(child, query, topic2question);
     }
+    return ret;
 }
 TreeNode* findTopic(TreeNode* root, std::string& topic){
     if(root == nullptr){
         return nullptr;
     }
+    if(root->topic == topic){
+        return root;
+    }
     
     for(TreeNode* child:root->children){
-        if(child->topic == topic){
-            return child;
-        }else{
-            return findTopic(child, topic);
+        TreeNode* ret = findTopic(child, topic);
+        if(ret != nullptr){
+            return ret;
         }
     }
     return nullptr;
 }
+
+std::vector<TreeNode*> buildTree(std::vector<std::string> topics, int& i){
+    std::vector<TreeNode*> ret;
+    if(i < topics.size() && topics[i] !="("){
+        //something wrong
+    }   
+    i++;
+    while(i < topics.size() && topics[i] != ")"){
+        TreeNode* node = new TreeNode(topics[i]);
+        if(i +1< topics.size() && topics[i+1] == "("){
+            i++;
+            node->children = buildTree(topics, i);
+        }
+        ret.push_back(node);
+        i++;
+    }
+    i++;
+    return ret;
+}
+
 std::vector<int> ontology(std::string treeRepr, std::vector<std::string> questions, std::vector<std::string> queries) 
 {
     using namespace std;
+    vector<string> topics;
+    stringstream ss(treeRepr);
+    copy(istream_iterator<string>(ss),
+        istream_iterator<string>(),
+        back_inserter(topics));
     
-    string token = "";
-    int i = 0;
     
-    TreeNode* root = new TreeNode("*");
-    for(char c: treeRepr){
-        
+    TreeNode* root = new TreeNode(topics[0]);
+    int i = 1;
+    root->children = buildTree(topics, i);
+    
+    map<string, vector<string>> topic2question;
+    for(string quesstr: questions){
+        size_t pos = quesstr.find(":");
+        string topic = quesstr.substr(0, pos);
+        string ques = quesstr.substr(pos+2);
+        topic2question[topic].push_back(ques);
     }
-    //build tree
-    root.children = buildTree(treeRepr, i);
+    vector<int> ret(queries.size(), 0);
     
-    map<string, TreeNode*> topic2question;
-    for(string ques: questions){
-        size_t pos = ques.find(":");
-        string topic = ques.substr(0, pos);
-        string q = ques.substr(pos+2);
-        topic2question[topic] = q;
-    }
-    vector<int>  ret(queries.size(), 0);
-    int i = 0; 
-    for(string query: queries){
-        size_t pos = query.find(" ");
-        string topic = ques.substr(0, pos);
-        string q = ques.substr(pos+1);
+    for(int k = 0; k < queries.size(); ++k){
+        string querystr = queries[k];
+        size_t pos = querystr.find(" ");
+        string topic = querystr.substr(0, pos);
+        string query = querystr.substr(pos+1);
         TreeNode* topicNode = findTopic(root, topic);
-        countQuery(topicNode, q, topic2question, ret,i);
-        i++;
+        ret[k] = countQuery(topicNode, query, topic2question);
     }
     return ret;
 }
