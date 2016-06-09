89b3196ca81a78d35cb4085d65a2c2acc09aaabe
atulkum <atulcst@gmail.com>
2016-06-09 12:17:41 -0700

    Update ontology.cpp

--- ontology.cpp
+++ ontology.cpp
@@ -1,3 +1,8 @@
+#include <map>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <sstream>
 struct TreeNode{
     std::string topic;
     std::vector<TreeNode*> children;
@@ -46,7 +51,7 @@ TreeNode* findTopic(TreeNode* root, std::string& topic){
     return nullptr;
 }
 
-std::vector<TreeNode*> buildTree(std::vector<std::string> topics, int& i){
+/*std::vector<TreeNode*> buildTree(std::vector<std::string> topics, int& i){
     std::vector<TreeNode*> ret;
     if(i < topics.size() && topics[i] !="("){
         //something wrong
@@ -63,22 +68,52 @@ std::vector<TreeNode*> buildTree(std::vector<std::string> topics, int& i){
     }
     i++;
     return ret;
+}*/
+TreeNode* buildTree(std::string treeRepr){
+    std::vector<std::string> topics;
+    std::stringstream ss(treeRepr);
+    std::copy(std::istream_iterator<std::string>(ss),
+        std::istream_iterator<std::string>(),
+        std::back_inserter(topics));
+
+    std::vector<TreeNode*> stack;
+    int i = 0;
+    for(int i =0; i <= topics.size(); ++i){
+        if(topics[i] == ")"){
+                std::vector<TreeNode*> children;
+                assert(stack.empty() == false);
+                while(stack.back()->topic != "("){
+                        children.push_back(stack.back());
+                        stack.pop_back();
+                }
+                stack.pop_back();
+                children.swap(stack.back()->children);
+        }else if(!topics[i].empty()){
+                TreeNode* node = new TreeNode(topics[i]);
+                stack.push_back(node);
+        }
+    }
+    assert(stack.size() == 1);
+    return stack[0];
 }
+
 
 std::vector<int> ontology(std::string treeRepr, std::vector<std::string> questions, std::vector<std::string> queries) 
 {
     using namespace std;
-    vector<string> topics;
+    /*vector<string> topics;
     stringstream ss(treeRepr);
     copy(istream_iterator<string>(ss),
         istream_iterator<string>(),
         back_inserter(topics));
     
-    
     TreeNode* root = new TreeNode(topics[0]);
     int i = 1;
     root->children = buildTree(topics, i);
-    
+    */
+
+    TreeNode* root =  buildTree(treeRepr);
+
     map<string, vector<string>> topic2question;
     for(string quesstr: questions){
         size_t pos = quesstr.find(":");
@@ -98,3 +133,31 @@ std::vector<int> ontology(std::string treeRepr, std::vector<std::string> questio
     }
     return ret;
 }
+
+int main(){
+    std::string treestr = "n ( nnnZ ( x ( nnxnn ( xnhnnnz nnnnn ) nnnZZn ) nnnnZnn ( nnnxn ( xn ( nennx ( nnnxnnn ) ) ) nnn ( nn ( nnnnnnn ( znnnnnnx ) ) ) ) nnnnZz ) nnnx nnnnx nnnn )";
+    std::vector<std::string> questions({"nennx: nnnnn", 
+ "nnnnZz: nnnn", 
+ "nnnnZz: nnnnnnnnn", 
+ "nnnnZz: nznn", 
+ "nnnZ: nnZn", 
+ "nnnnZnn: nZn", 
+ "nnnnZz: nnnnnn", 
+ "nnnxnnn: nnnnn", 
+ "znnnnnnx: nnnnxnnnn", 
+ "x: nn", 
+ "nnnxn: nxnnnnn", 
+ "nnnnZz: nnnnxnn", 
+ "xn: nnnnnnnnn", 
+ "nnn: nnnnnn", 
+ "nnnn: nn", 
+ "nnn: nnnZnnzn", 
+ "nnnn: xn", 
+ "nnxnn: nnnnxnnn", 
+ "nennx: nnnnZnnn", 
+ "xnhnnnz: nn"});
+    std::vector<std::string> queries({"nnnZ n"});
+	auto ret = ontology(treestr, questions, queries);
+	std::cout << ret[0] << std::endl;
+}
+
