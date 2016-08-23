e85c6e2cbdeab6769a840ba46a07d65b42e65996
Atul Kumar <atulcst@gmail.com>
2016-08-23 10:45:19 -0700

    Create union_find.cpp

---
+++ union_find.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+using namespace std;
+
+class UnionFind{
+private:
+  vector<int> Arr;
+  vector<int> size;
+  int N;
+public:
+  UnionFind(int _N): Arr(N), size(N), N(_N){
+    initialize();
+  }
+  void initialize(){
+    for(int i = 0;i<N;i++){
+      Arr[ i ] = i ;
+      size[ i ] = 1;
+    }
+  }
+
+  void weighted-union(int A,int B){
+    int root_A = root(A);
+    int root_B = root(B);
+    if(size[root_A] < size[root_B ]){
+      Arr[ root_A ] = Arr[root_B];
+      size[root_B] += size[root_A];
+    }else{
+      Arr[ root_B ] = Arr[root_A];
+      size[root_A] += size[root_B];
+    }
+  }
+  int root (int i){
+    while(Arr[ i ] != i){
+      Arr[ i ] = Arr[ Arr[ i ] ] ; 
+      i = Arr[ i ]; 
+    }
+    return i;
+  }
+
+  bool find(int A,int B){
+    return root(A)==root(B);
+  }
+};
