d48a3694ce6b10c35d44855d02514265e3036fdb
Atul Kumar <atulcst@gmail.com>
2017-10-08 11:50:19 -0700

    Update graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -42,3 +42,50 @@ bool hasDeadlock(std::vector<std::vector<int>> connections) {
   }
   return false;
 }
+
+class Graph{
+public:
+  std::map<int, int> disc;
+  std::map<int, int> low;
+  std::set<int> visited;
+  std::map<int, int> parent;
+  
+  std::vector<std::vector<int>> conn;
+  int ts;
+  int ret;
+
+  Graph(std::vector<std::vector<int>>& a_conn){
+    conn = a_conn;
+    ts = 0;
+    ret = 0;
+  }
+  
+  void dfs(int st){
+    disc[st] = ts;
+    low[st] = ts;
+    ts++;
+    visited.insert(st);
+    for(int c = 0; c < conn.size(); ++c){
+      if(conn[st][c] == 0) continue; 
+      if(visited.count(c) == 0){
+        parent[c] = st;
+        dfs(c);
+        if(low[c] > disc[st]){
+          ret++;
+        }
+      }
+      if(parent.count(st) == 0 || c != parent[st]){
+        low[st] = std::min(low[st], low[c]);
+      }
+    }
+    
+  }
+};
+
+
+int singlePointOfFailure(std::vector<std::vector<int>> connections) {
+  Graph g(connections);
+  g.dfs(0);
+  return g.ret;
+}
+
